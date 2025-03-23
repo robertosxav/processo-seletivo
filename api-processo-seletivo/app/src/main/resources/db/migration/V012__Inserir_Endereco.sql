2373e23875944b24017223523096ae436bc1b7fb
robertosxav <roberto.sxav@gmail.com>
2025-03-23 14:47:09 -0400

    script para inserir cidades e enderecos

---
+++ api-processo-seletivo/app/src/main/resources/db/migration/V012__Inserir_Endereco.sql
@@ -0,0 +1,6 @@
+INSERT INTO public.endereco (end_tipo_logradouro, end_logradouro, end_numero, end_bairro, cid_id) VALUES
+('Avenida', 'Paulista', 1578, 'Bela Vista', 4),
+('Rua', 'Visconde de Pirajá', 430, 'Ipanema', 5),
+('Avenida', 'Afonso Pena', 1200, 'Centro', 6),
+('Rua', 'Chile', 25, 'Centro Histórico', 7),
+('Avenida', 'Batel', 900, 'Batel', 8);
