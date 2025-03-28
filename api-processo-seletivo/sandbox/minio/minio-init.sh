7a843e91c7663f54e9e2bd8d3d943bfc59dd821d
Roberto de Souza Xavier Junior <robertojunior@seplag.mt.gov.br>
2025-03-28 17:08:28 -0400

    Normalizando finais de linha para LF

--- api-processo-seletivo/sandbox/minio/minio-init.sh
+++ api-processo-seletivo/sandbox/minio/minio-init.sh
@@ -1,33 +1,14 @@
 #!/bin/sh
 
 
-echo "Aguardando MinIO iniciar..."
-sleep 30
-COUNTER=0
-MAX_RETRIES=20
+echo "Setting permissions for MinIO initialization script..."
+chmod +x /minio/minio-init.sh
 
-while ! mc alias set myminio http://minio:9000 admin seletivo123@; do
-  echo "MinIO ainda não está pronto... aguardando..."
-  sleep 2
-  COUNTER=$((COUNTER + 1))
-  if [ "$COUNTER" -ge "$MAX_RETRIES" ]; then
-    echo "Erro: MinIO não iniciou após várias tentativas."
-    exit 1
-  fi
-done
+echo "Waiting for MinIO server to start..."
+sleep 5
 
-echo "MinIO iniciado! Configurando MinIO Client..."
+echo "Executing MinIO initialization..."
+/minio/minio-init.sh &
 
-# Criar bucket fotos
-if mc ls myminio/fotos >/dev/null 2>&1; then
-  echo "Bucket 'fotos' já existe."
-else
-  echo "Criando bucket 'fotos'..."
-  mc mb myminio/fotos
-  if [ $? -eq 0 ]; then
-    echo "Bucket 'fotos' criado com sucesso!"
-  else
-    echo "Erro ao criar bucket!"
-    exit 1
-  fi
-fi
+echo "Starting MinIO server..."
+exec minio server --console-address :9001 /data
